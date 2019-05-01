f4b8cee8cabc21c190dd1585d1894b0cefa4f427
masx200 <34191203+masx200@users.noreply.github.com>
2019-05-01 11:42:49 +0800

    commit

--- src/my-vue-router-project/myajaxget.js
+++ src/my-vue-router-project/myajaxget.js
@@ -1,53 +1,52 @@
 haverun = 0
 setTimeout(() => {
 
-    window.onload()
+	window.onload()
 
 }, 1000)
 window.onload = () => {
-    if (haverun == 0) {
-        haverun = 1
-        console.log("开始运行此onload函数")
-        myonloadfunc()
-    }
-    else {
-        console.log("不要重复运行此onload函数")
-    }
+	if (haverun == 0) {
+		haverun = 1
+		console.log("开始运行此onload函数")
+		myonloadfunc()
+	} else {
+		console.log("不要重复运行此onload函数")
+	}
 
-    // myonload1()
-    // myonload2()
+	// myonload1()
+	// myonload2()
 }
-myonloadfunc=myonload1
+myonloadfunc = myonload1
 // console.log($("#App").html() =="", $("#about").html()=="" , $("#home").html()=="" , $("#huami").html()=="")
 function myonload1() {
-    allajaxsuccess = 0
-    // console.log($("#App").html())
-    function mynextfun1(responseTxt, statusTxt, xhr) {
-// console.log(window.myxhr=xhr)
-        if (statusTxt == "success")
-            console.log("外部内容加载成功!\n" + responseTxt);
-        allajaxsuccess++
-        if (statusTxt == "error")
-            console.log("Error: " + xhr.status + ": " + xhr.statusText);
-        // console.log($("#App").html() ==""|| $("#about").html()=="" || $("#home").html()=="" || $("#huami").html()=="")
-        // console.log("ajaxget,ok!")
-        if (allajaxsuccess < 4) {
-            console.log("外部内容加载部分未完成")
+	allajaxsuccess = 0
+	// console.log($("#App").html())
+	function mynextfun1(responseTxt, statusTxt, xhr) {
+		// console.log(window.myxhr=xhr)
+		if (statusTxt == "success")
+			console.log("外部内容加载成功!\n" + responseTxt);
+		allajaxsuccess++
+		if (statusTxt == "error")
+			console.log("Error: " + xhr.status + ": " + xhr.statusText);
+		// console.log($("#App").html() ==""|| $("#about").html()=="" || $("#home").html()=="" || $("#huami").html()=="")
+		// console.log("ajaxget,ok!")
+		if (allajaxsuccess < 4) {
+			console.log("外部内容加载部分未完成")
 
-        } else {
-            console.log("外部内容加载全部完成")
-            myonload2()
-        }
+		} else {
+			console.log("外部内容加载全部完成")
+			myonload2()
+		}
 
-    }
-    console.log("开始外部内容加载");
-    myarrayselector = ["#App", "#about", "#home", "#huami"]
-    myarrayselector.forEach(element => {
-        console.log("开始加载" + $(element).attr("src"));
-        $(element).load($(element).attr("src"), mynextfun1);
-    });
-    // $("#App").load("app.template.html", mynextfun1);
-    // $("#about").load("about.template.html", mynextfun1);
-    // $("#home").load("home.template.html", mynextfun1);
-    // $("#huami").load("huami.template.html", mynextfun1);
+	}
+	console.log("开始外部内容加载");
+	myarrayselector = ["#App", "#about", "#home", "#huami"]
+	myarrayselector.forEach(element => {
+		console.log("开始加载" + $(element).attr("src"));
+		$(element).load($(element).attr("src"), mynextfun1);
+	});
+	// $("#App").load("app.template.html", mynextfun1);
+	// $("#about").load("about.template.html", mynextfun1);
+	// $("#home").load("home.template.html", mynextfun1);
+	// $("#huami").load("huami.template.html", mynextfun1);
 }
--- src/花密网页版-完善修改版-响应式的导航栏/huami+clipboard.js
+++ src/花密网页版-完善修改版-响应式的导航栏/huami+clipboard.js
@@ -1,82 +1,83 @@
-  function countCode() {
-            var password = $("#password").val();
-            var key = $("#key").val();
-            if (password && key) {
-                var md5one = $.md5(password, key);
-                var md5two = $.md5(md5one, 'snow');
-                var md5three = $.md5(md5one, 'kise');
+function countCode() {
+  	var password = $("#password").val();
+  	var key = $("#key").val();
+  	if (password && key) {
+  		var md5one = $.md5(password, key);
+  		var md5two = $.md5(md5one, 'snow');
+  		var md5three = $.md5(md5one, 'kise');
 
-                var rule = md5three.split("");
-                var source = md5two.split("");
-                for (var i = 0; i <= 31; i++) {
-                    if (isNaN(source[i])) {
-                        str = "sunlovesnow1990090127xykab";
-                        if (str.search(rule[i]) > -1) {
-                            source[i] = source[i].toUpperCase();
-                        }
-                    }
-                }
-                var code32 = source.join("");
-                var code1 = code32.slice(0, 1);
-                if (isNaN(code1)) {
-                    var code16 = code32.slice(0, 16);
-                } else {
-                    var code16 = "K" + code32.slice(1, 16);
-                }
+  		var rule = md5three.split("");
+  		var source = md5two.split("");
+  		for (var i = 0; i <= 31; i++) {
+  			if (isNaN(source[i])) {
+  				str = "sunlovesnow1990090127xykab";
+  				if (str.search(rule[i]) > -1) {
+  					source[i] = source[i].toUpperCase();
+  				}
+  			}
+  		}
+  		var code32 = source.join("");
+  		var code1 = code32.slice(0, 1);
+  		if (isNaN(code1)) {
+  			var code16 = code32.slice(0, 16);
+  		} else {
+  			var code16 = "K" + code32.slice(1, 16);
+  		}
 
-                $("#code16").val(code16);
-            }
-        }
-        $(function () {
+  		$("#code16").val(code16);
+  	}
+  }
+  $(function() {
 
-            $('#password').change(countCode);
-            $('#key').change(countCode);
-            $('#password').keyup(countCode);
-            $('#key').keyup(countCode);
+  	$('#password').change(countCode);
+  	$('#key').change(countCode);
+  	$('#password').keyup(countCode);
+  	$('#key').keyup(countCode);
 
-            $('#code16').mouseover(function () {
-                $('#copycode16').addClass("copycode16h");
-                $('#code16').addClass("code16h");
+  	$('#code16').mouseover(function() {
+  		$('#copycode16').addClass("copycode16h");
+  		$('#code16').addClass("code16h");
 
-            })
-            $('#code16').mouseout(function () {
-                $('#copycode16').removeClass("copycode16h");
-                $('#code16').removeClass("code16h");
-            })
-            $('#copycode16').mouseover(function () {
-                $('#copycode16').addClass("copycode16h");
-                $('#code16').addClass("code16h");
-            })
-            $('#copycode16').mouseout(function () {
-                $('#copycode16').removeClass("copycode16h");
-                $('#code16').removeClass("code16h");
-            })
-            $('#copycode16').click(function () {
-                if ($("#code16").val()) {
-                    $('#copyOK').show();
-                    $('#copyOK').fadeTo(0, 0).css("border-color", "#22B614").css("background-color", "#22B614").fadeTo('normal', 1).fadeTo(2000, 1).fadeTo(3000, 0, function () {
-                        $('#copyOK').hide();
-                    });
-                }
+  	})
+  	$('#code16').mouseout(function() {
+  		$('#copycode16').removeClass("copycode16h");
+  		$('#code16').removeClass("code16h");
+  	})
+  	$('#copycode16').mouseover(function() {
+  		$('#copycode16').addClass("copycode16h");
+  		$('#code16').addClass("code16h");
+  	})
+  	$('#copycode16').mouseout(function() {
+  		$('#copycode16').removeClass("copycode16h");
+  		$('#code16').removeClass("code16h");
+  	})
+  	$('#copycode16').click(function() {
+  		if ($("#code16").val()) {
+  			$('#copyOK').show();
+  			$('#copyOK').fadeTo(0, 0).css("border-color", "#22B614").css("background-color", "#22B614").fadeTo('normal', 1)
+  				.fadeTo(2000, 1).fadeTo(3000, 0, function() {
+  					$('#copyOK').hide();
+  				});
+  		}
 
-            })
+  	})
 
-        });
-   // </script>
+  });
+  // </script>
   //  <script defer>
-        $(function () {
-            var clipboard = new ClipboardJS('.btn');
+  $(function() {
+  	var clipboard = new ClipboardJS('.btn');
 
-            clipboard.on('success', function (e) {
-                console.info('Action:', e.action);
-                console.info('Text:', e.text);
+  	clipboard.on('success', function(e) {
+  		console.info('Action:', e.action);
+  		console.info('Text:', e.text);
 
-                e.clearSelection();
-            });
+  		e.clearSelection();
+  	});
 
-            clipboard.on('error', function (e) {
-                console.error('Action:', e.action);
-                console.error('Trigger:', e.trigger);
-            });
-            $('#copyOK').hide();
-        })
+  	clipboard.on('error', function(e) {
+  		console.error('Action:', e.action);
+  		console.error('Trigger:', e.trigger);
+  	});
+  	$('#copyOK').hide();
+  })
