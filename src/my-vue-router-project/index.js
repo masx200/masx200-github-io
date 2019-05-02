bce60dcf50521d58c4949e0b8fcbd704f6e42a4f
masx200 <34191203+masx200@users.noreply.github.com>
2019-05-02 22:29:34 +0800

    react项目增加rssreader测试

--- src/my-vue-router-project/index.js
+++ src/my-vue-router-project/index.js
@@ -1,3 +1,4 @@
+// window.Vue=Vue
 //alert("webapck test");
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
