3d4d6ff1ed8b62f0a720c647d6f6bdb5b45e84e7
masx200 <34191203+masx200@users.noreply.github.com>
2019-05-23 11:55:41 +0800

    Update 点击链接不跳转-修改当前的网页地址-动态加载网页内容不刷新.js

--- src/点击链接不跳转-修改当前的网页地址-动态加载网页内容不刷新.js
+++ src/点击链接不跳转-修改当前的网页地址-动态加载网页内容不刷新.js
@@ -1,9 +1,20 @@
 (global => {
   /* 注意不能跨域 */
-  /* 注意:对于使用了document.write的网站, 加载会出错,因为脚本都是异步加载的,网页内容会被覆盖*/
-
+  /* 注意:对于使用了document.write的网站, 加载会出错,因为脚本都是异步加载的,网页内容会被覆盖
+  修改了document.write方法
+  */
+  if (
+    typeof 点击链接不跳转修改当前的网页地址动态加载网页内容不刷新 !== "function"
+  ) {
+    window.addEventListener("load", windowloadhandler);
+  }
+  /**
+   * 防止多次加载windowloadhandler
+   *
+   *   *
+   */
   global.点击链接不跳转修改当前的网页地址动态加载网页内容不刷新 = windowloadhandler;
-  window.addEventListener("load", windowloadhandler);
+
   function windowloadhandler() {
     var loadid;
     if (!importScripts) {
