0345412a4bbd0a059d75eec8815565f0a3fd7aef
masx200 <34191203+masx200@users.noreply.github.com>
2019-05-27 11:47:17 +0800

    Update importcjsamdumd.js

--- src/importcjsamdumd.js
+++ src/importcjsamdumd.js
@@ -7,10 +7,110 @@
     module.exports = importcjsamdumd;
   }
   global.IMPORTCJSAMDUMD = importcjsamdumd;
+  importcjsamdumd.REQUIREPACKAGE= require;
   importcjsamdumd.GLOBALPACKAGESTORE = importcjsamdumd.GLOBALPACKAGESTORE || [];
+  function require(packagename=undefined) {
+      var findpackage= importcjsamdumd.GLOBALPACKAGESTORE[packagename]
+      if(findpackage){
+        return findpackage;
+      }else{
+          throw new Error("Cannot find module  "+ packagename )
+      }
+    
+  }
+  function define(name, deps, callback) {
+    define.globalDefQueue = [];
+    //   window.globalDefQueue = globalDefQueue;
+    var op = Object.prototype;
+    var ostring = op.toString;
+    var useInteractive = false;
+    function isArray(it) {
+      return ostring.call(it) === "[object Array]";
+    }
+    function isFunction(it) {
+      return ostring.call(it) === "[object Function]";
+    }
+    var node, context;
+
+    //Allow for anonymous modules
+    if (typeof name !== "string") {
+      //Adjust args appropriately
+      callback = deps;
+      deps = name;
+      name = null;
+    }
+
+    //This module may not have dependencies
+    if (!isArray(deps)) {
+      callback = deps;
+      deps = null;
+    }
+
+    //If no name, and callback is a function, then figure out if it a
+    //CommonJS thing with dependencies.
+    if (!deps && isFunction(callback)) {
+      deps = [];
+      //Remove comments from the callback string,
+      //look for require calls, and pull them into the dependencies,
+      //but only if there are function args.
+      if (callback.length) {
+        callback
+          .toString()
+          .replace(commentRegExp, commentReplace)
+          .replace(cjsRequireRegExp, function(match, dep) {
+            deps.push(dep);
+          });
+
+        //May be a CommonJS thing even without require calls, but still
+        //could use exports, and module. Avoid doing exports and module
+        //work though if it just needs require.
+        //REQUIRES the function to expect the CommonJS variables in the
+        //order listed below.
+        deps = (callback.length === 1
+          ? ["require"]
+          : ["require", "exports", "module"]
+        ).concat(deps);
+      }
+    }
+
+    //If in IE 6-8 and hit an anonymous define() call, do the interactive
+    //work.
+    if (useInteractive) {
+      node = currentlyAddingScript || getInteractiveScript();
+      if (node) {
+        if (!name) {
+          name = node.getAttribute("data-requiremodule");
+        }
+        context = contexts[node.getAttribute("data-requirecontext")];
+      }
+    }
+
+    //Always save off evaluating the def call until the script onload handler.
+    //This allows multiple modules to be in a file without prematurely
+    //tracing dependencies, and allows for anonymous module support,
+    //where the module name is not known until the script onload event
+    //occurs. If no context, use the global queue, and get it processed
+    //in the onscript load callback.
+    if (context) {
+      context.defQueue.push([name, deps, callback]);
+      context.defQueueMap[name] = true;
+    } else {
+      define.globalDefQueue.push([name, deps, callback]);
+    }
+    console.log(define.globalDefQueue[0]);
+    if (typeof define.globalDefQueue[0][0] === "string") {
+      packagename = define.globalDefQueue[0][0];
+    }
+    var canshu = define.globalDefQueue[0][1].map(e => require(e));
+    //   console.log(canshu);
+    define.exports = define.globalDefQueue[0][2](...canshu);
+  }
+
+  define.amd = true;
   function importcjsamdumd(url, packagename = undefined) {
     //   window.GLOBALPACKAGESTORE = window.GLOBALPACKAGESTORE || [];
     url = new URL(url);
+
     return new Promise((resolve, reject) => {
       try {
         (async () => {
@@ -75,99 +175,7 @@
           };
           define.exports = {};
           // var globalDefQueue = [];
-          function define(name, deps, callback) {
-            define.globalDefQueue = [];
-            //   window.globalDefQueue = globalDefQueue;
-            var op = Object.prototype;
-            var ostring = op.toString;
-            var useInteractive = false;
-            function isArray(it) {
-              return ostring.call(it) === "[object Array]";
-            }
-            function isFunction(it) {
-              return ostring.call(it) === "[object Function]";
-            }
-            var node, context;
 
-            //Allow for anonymous modules
-            if (typeof name !== "string") {
-              //Adjust args appropriately
-              callback = deps;
-              deps = name;
-              name = null;
-            }
-
-            //This module may not have dependencies
-            if (!isArray(deps)) {
-              callback = deps;
-              deps = null;
-            }
-
-            //If no name, and callback is a function, then figure out if it a
-            //CommonJS thing with dependencies.
-            if (!deps && isFunction(callback)) {
-              deps = [];
-              //Remove comments from the callback string,
-              //look for require calls, and pull them into the dependencies,
-              //but only if there are function args.
-              if (callback.length) {
-                callback
-                  .toString()
-                  .replace(commentRegExp, commentReplace)
-                  .replace(cjsRequireRegExp, function(match, dep) {
-                    deps.push(dep);
-                  });
-
-                //May be a CommonJS thing even without require calls, but still
-                //could use exports, and module. Avoid doing exports and module
-                //work though if it just needs require.
-                //REQUIRES the function to expect the CommonJS variables in the
-                //order listed below.
-                deps = (callback.length === 1
-                  ? ["require"]
-                  : ["require", "exports", "module"]
-                ).concat(deps);
-              }
-            }
-
-            //If in IE 6-8 and hit an anonymous define() call, do the interactive
-            //work.
-            if (useInteractive) {
-              node = currentlyAddingScript || getInteractiveScript();
-              if (node) {
-                if (!name) {
-                  name = node.getAttribute("data-requiremodule");
-                }
-                context = contexts[node.getAttribute("data-requirecontext")];
-              }
-            }
-
-            //Always save off evaluating the def call until the script onload handler.
-            //This allows multiple modules to be in a file without prematurely
-            //tracing dependencies, and allows for anonymous module support,
-            //where the module name is not known until the script onload event
-            //occurs. If no context, use the global queue, and get it processed
-            //in the onscript load callback.
-            if (context) {
-              context.defQueue.push([name, deps, callback]);
-              context.defQueueMap[name] = true;
-            } else {
-              define.globalDefQueue.push([name, deps, callback]);
-            }
-            console.log(define.globalDefQueue[0]);
-            if (typeof define.globalDefQueue[0][0] === "string") {
-              packagename = define.globalDefQueue[0][0];
-            }
-            var canshu = define.globalDefQueue[0][1].map(e => require(e));
-            //   console.log(canshu);
-            define.exports = define.globalDefQueue[0][2](...canshu);
-          }
-
-          define.amd = true;
-
-          function require(packagename) {
-            return importcjsamdumd.GLOBALPACKAGESTORE[packagename];
-          }
           var exportmodule = (function(
             require,
             define,
