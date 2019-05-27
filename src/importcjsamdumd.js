02996708cd789b9b577b8efd0d98c30d11ee9480
masx200 <34191203+masx200@users.noreply.github.com>
2019-05-27 10:07:20 +0800

    优化了模块的全局定义

--- src/importcjsamdumd.js
+++ src/importcjsamdumd.js
@@ -1,8 +1,9 @@
 //包装cjs和amd和umd模块为异步加载promise方法
 
 //就像es6模块的import函数返回promise对象一样
-
-function importcjsamdumd(url) {
+// window . importcjsamdumd= importcjsamdumd
+function importcjsamdumd(url, packagename = undefined) {
+  window.globalpackagestore = window.globalpackagestore || [];
   url = new URL(url);
   return new Promise((resolve, reject) => {
     try {
@@ -66,9 +67,95 @@ function importcjsamdumd(url) {
         var module = {
           exports: {}
         };
-        function define(...args) {}
+        // var globalDefQueue = [];
+        function define(name, deps, callback) {
+          define.globalDefQueue = [];
+          //   window.globalDefQueue = globalDefQueue;
+          var op = Object.prototype;
+          var ostring = op.toString;
+          var useInteractive = false;
+          function isArray(it) {
+            return ostring.call(it) === "[object Array]";
+          }
+          function isFunction(it) {
+            return ostring.call(it) === "[object Function]";
+          }
+          var node, context;
+
+          //Allow for anonymous modules
+          if (typeof name !== "string") {
+            //Adjust args appropriately
+            callback = deps;
+            deps = name;
+            name = null;
+          }
+
+          //This module may not have dependencies
+          if (!isArray(deps)) {
+            callback = deps;
+            deps = null;
+          }
+
+          //If no name, and callback is a function, then figure out if it a
+          //CommonJS thing with dependencies.
+          if (!deps && isFunction(callback)) {
+            deps = [];
+            //Remove comments from the callback string,
+            //look for require calls, and pull them into the dependencies,
+            //but only if there are function args.
+            if (callback.length) {
+              callback
+                .toString()
+                .replace(commentRegExp, commentReplace)
+                .replace(cjsRequireRegExp, function(match, dep) {
+                  deps.push(dep);
+                });
+
+              //May be a CommonJS thing even without require calls, but still
+              //could use exports, and module. Avoid doing exports and module
+              //work though if it just needs require.
+              //REQUIRES the function to expect the CommonJS variables in the
+              //order listed below.
+              deps = (callback.length === 1
+                ? ["require"]
+                : ["require", "exports", "module"]
+              ).concat(deps);
+            }
+          }
+
+          //If in IE 6-8 and hit an anonymous define() call, do the interactive
+          //work.
+          if (useInteractive) {
+            node = currentlyAddingScript || getInteractiveScript();
+            if (node) {
+              if (!name) {
+                name = node.getAttribute("data-requiremodule");
+              }
+              context = contexts[node.getAttribute("data-requirecontext")];
+            }
+          }
+
+          //Always save off evaluating the def call until the script onload handler.
+          //This allows multiple modules to be in a file without prematurely
+          //tracing dependencies, and allows for anonymous module support,
+          //where the module name is not known until the script onload event
+          //occurs. If no context, use the global queue, and get it processed
+          //in the onscript load callback.
+          if (context) {
+            context.defQueue.push([name, deps, callback]);
+            context.defQueueMap[name] = true;
+          } else {
+            define.globalDefQueue.push([name, deps, callback]);
+          }
+          console.log(define.globalDefQueue[0]);
+          define.exports = define.globalDefQueue[0][2]();
+        }
+
         define.amd = true;
-        function require(m) {}
+
+        function require(packagename) {
+          return window.globalpackagestore[packagename];
+        }
         var exportmodule = (function(
           require,
           define,
@@ -81,18 +168,56 @@ function importcjsamdumd(url) {
           //     module[__key__]=module.exports[__key__]
           // }
 
-          var moduleexport = {};
-          console.log("exports",exports,"module.exports",module.exports)
-        //   console.log()
-          if (Object.keys(exports).length) {
-            moduleexport.default = exports;
-          } else if (Object.keys(module.exports).length) {
-            moduleexport.default = module.exports;
-          }
-          return moduleexport;
-        })(require, define, module, exports, scripttext);
+          //   var moduleexport = {};
+          //   console.log("exports", exports, "module.exports", module.exports);
+          //   //   console.log()
+          //   if (Object.keys(exports).length) {
+          //     moduleexport.default = exports;
+          //   } else if (Object.keys(module.exports).length) {
+          //     moduleexport.default = module.exports;
+          //   }
 
-        resolve(exportmodule);
+          //   console.log(
+          //     "exports",
+          //     exports,
+          //     "module.exports",
+          //     module.exports,
+          //     // "globalDefQueue[2]",
+          //     // globalDefQueue[2]
+          //   );
+
+          return [exports, module.exports];
+        })(require, define, module, exports, scripttext);
+        // console.log(define.exports);
+        // console.log(exportmodule);
+        // exports = exportmodule[0];
+        // module.exports = exportmodule[1];
+        var moduleexport = {};
+        // console.log(
+        //   "exports",
+        //   exports,
+        //   "module.exports",
+        //   module.exports,
+        // //   "globalDefQueue[2]",
+        // //   globalDefQueue[2]
+        // );
+        // console.log(exportmodule[1]);
+        if (Object.keys(exportmodule[0]).length) {
+          moduleexport.default = exportmodule[0];
+        } else if (Object.keys(exportmodule[1]).length) {
+          moduleexport.default = exportmodule[1];
+        } else if (
+          typeof define.exports !== "undefined"
+          //    ||
+          //   Object.keys(define.exports).length
+        ) {
+          moduleexport.default = define.exports;
+        }
+        if (typeof packagename !== "undefined") {
+          window.globalpackagestore[packagename] = moduleexport.default;
+        }
+
+        resolve(moduleexport);
       })();
     } catch (e) {
       console.error(e);
