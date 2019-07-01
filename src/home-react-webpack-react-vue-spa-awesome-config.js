b8ff6bc1373594c2624d2a6c6e21001c9cb7ce7c
masx200 <34191203+masx200@users.noreply.github.com>
2019-07-01 09:09:57 +0800

    Update home-react-webpack-react-vue-spa-awesome-config.js

--- src/home-react-webpack-react-vue-spa-awesome-config.js
+++ src/home-react-webpack-react-vue-spa-awesome-config.js
@@ -2,7 +2,7 @@ import Markdownreact from "./markdown-react/index";
 import React, { useEffect, useState, useRef } from "react";
 export default function() {
   useEffect(() => {
-    document.title =
+    document.title ="masx200的github主页"+
       "webpack-react-vue-spa-awesome-config 极速、零配置的 web 应用打包工具, 同时支持 react 和 vue 的单页面应用,提供开箱即用支持";
   }, []);
   return (
