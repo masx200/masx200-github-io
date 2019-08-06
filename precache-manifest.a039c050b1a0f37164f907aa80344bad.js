be69e0c911dcfed21c7e164d6d0b52fb059afd43
masx200 <34191203+masx200@users.noreply.github.com>
2019-08-06 23:49:05 +0800

    添加fetch的polyfill

---
+++ precache-manifest.a039c050b1a0f37164f907aa80344bad.js
@@ -0,0 +1,99 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/babeljs.a3109f62b30ba938cca6dd8e654cf8a9.png"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/bundle.runtime~main.8441f8a08b592020145c.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.0.4c5c6bd444606c2e8d00.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.1.41813f625025b682940c.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.10.81cb2b99fd064bfa8648.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.11.edcfd47234fbb0aa567a.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.12.3a6211b87fb236adc8f7.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.13.ec307037498638bf70ef.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.14.9a3997ee4770ea7a9091.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.15.a722e3038d0ff642c855.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.16.8e98397d709ab7ad4062.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.17.0d3a69342d6e4a253407.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.18.155639e7c91d30fdddc9.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.19.b207c3470e386802c6be.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.2.5dea23f9e69b3d663523.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.20.3c466df5d7b62adbfafb.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.21.3ccb36524ce817ecd843.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.22.81155a52cd0c9e59ece0.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.23.bca7c068e6c21c777fc1.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.3.3b5432079fc6ef3db2ff.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.6.923827213ca505ab23dc.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.7.bcc8c26eb1924327fc29.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.8.d17a0058835bd683a747.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.9.c0472fd674bdec65b9c1.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/chunk.main.c228e2770a42fabe3cf2.js"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/main.c228e2770a42fabe3cf2.css"
+  },
+  {
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/parcel.9dc2fb9f99082fe3a958daf07adb4e9b.jpg"
+  },
+  {
+    "revision": "a42225eee8a50e79082c03b8087b2586",
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/service-worker-hieroglyphy.worker.1ec889f1899f5cec720b.worker.js"
+  },
+  {
+    "revision": "c43e604eac6157c64ed1c8ec06439a29",
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/service-worker-jsfuck.worker.47213a37bde72d0055c6.worker.js"
+  },
+  {
+    "revision": "b4349432150e4c28452ab01f81b0983b",
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/service-worker-mythread1-bigint.worker.e5964a62e9233cfdef36.worker.js"
+  },
+  {
+    "revision": "6d5cf5635115303c79c3175a71e612a5",
+    "url": "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@latest/service-worker-mythread1-decimal.worker.d1fe7b57b21090bee2a3.worker.js"
+  }
+]);
