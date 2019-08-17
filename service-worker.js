1dc1911858f8cea3a8e361bb0075d3f9c35850b0
masx200 <34191203+masx200@users.noreply.github.com>
2019-08-17 14:40:14 +0800

    升级更新优化

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.2.16/precache-manifest.f4fa0b9a7f7e3c5284e9178c80245498.js"
+  "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.3.0/precache-manifest.4636ca655701797622e9c89fcc48b202.js"
 );
 
 self.addEventListener('message', (event) => {
