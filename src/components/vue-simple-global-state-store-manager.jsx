66dd92c8717c4d52372ab1f09ee3b1386c586c48
masx200 <34191203+masx200@users.noreply.github.com>
2019-08-19 18:16:52 +0800

    Update vue-simple-global-state-store-manager.jsx

--- src/components/vue-simple-global-state-store-manager.jsx
+++ src/components/vue-simple-global-state-store-manager.jsx
@@ -1,1 +1,41 @@
 
+"use strict";
+// eslint-disable-next-line no-unused-vars
+import Markdownreact from "./markdown-react/index";
+
+// var React = window.IMPORTCJSAMDUMD.REQUIREPACKAGE("react");
+import React from "react";
+var {
+  useEffect
+  // , useState
+} = React;
+/* 把class组件改成function组件 */
+const urlmark =
+  "https://masx200.github.io/vue-simple-global-state-store-manager/README.md";
+export default React.memo(component);
+
+function component() {
+  // const [urlmark, seturlmark] = useState(
+  //   "https://masx200.github.io/excellent-vscode-extensions-for-javascript/README.md"
+  // );
+  useEffect(function() {
+  
+/* 
+ document.title =
+      "masx200的github主页-" +
+      `   excellent-vscode-extensions-for-javascript  适合于javascript/html/css/vue/reqct编程的vscode的优秀扩展推荐和vscode 的推荐设置 `;
+ 
+*/
+
+ }, []);
+
+  return (
+    <div
+    // onClick={() => {
+    //   seturlmark("https://masx200.github.io/README.md");
+    // }}
+    >
+      <Markdownreact src={urlmark} />
+    </div>
+  );
+}
