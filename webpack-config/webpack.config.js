5ea2ef9fe212939b7f66ba173710af3aa3ffd282
masx200 <34191203+masx200@users.noreply.github.com>
2019-10-13 23:30:27 +0800

    升级了@masx200/webpack-react-vue-spa-awesome-config

--- webpack-config/webpack.config.js
+++ webpack-config/webpack.config.js
@@ -1,24 +1,11 @@
+// @ts-nocheck
 const path = require("path");
-// const { default: InjectPlugin } = require("webpack-inject-plugin");
-/* --output-public-path=https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.3.6/  */
-// @ts-ignore
 const { version } = require("../package.json");
-// console.log(version);
 const webpackconfig = require("@masx200/webpack-react-vue-spa-awesome-config");
 if ("production" === process.env.NODE_ENV) {
   webpackconfig.output.publicPath = `https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@${version}/`;
 }
-const { /* plugins, */ module: webpackmodule } = webpackconfig;
-// const alias = webpackconfig.resolve.alias;
-
-/* webpackconfig.resolve.alias = {
-  ...alias,
-  ...{
-    react: "anujs",
-    "react-dom": "anujs"
-  }
-}; */
-
+const { module: webpackmodule } = webpackconfig;
 const randomstring = Math.floor(Math.random() * 10000000);
 const bannercontentloader = {
   exclude: [/node_modules/],
@@ -48,23 +35,12 @@ const webpackreplaceloader = {
     }
   ]
 };
-/* const myinjectplugin = new InjectPlugin(
-  function() {
-    return ` var ReactcreateElement=require('react').createElement;
-    console.log(ReactcreateElement);
-    `;
-  },
-  { entryName: () => true }
-); */
 const { rules: rules } = webpackmodule;
-/* webpack的loader是从后往前执行 */
-// @ts-ignore
 webpackconfig.module.rules = [
   webpackreplaceloader,
   ...rules,
   bannercontentloader
 ];
-// webpackconfig.plugins = [...plugins /* myinjectplugin */];
 const babelloaderrule = rules.find(loader => {
   return loader.loader === require.resolve("babel-loader");
 });
@@ -72,5 +48,4 @@ if (babelloaderrule) {
   babelloaderrule.include = [path.resolve("./")];
   babelloaderrule.exclude = [];
 }
-/* 让babel-loader转换所有文件 */
 module.exports = webpackconfig;
