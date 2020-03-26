21d6c219bade3d72fb7de69168f4f53f3cf477a4
masx200 <masx200@qq.c60>
2020-03-26 21:36:53 +0800

    集中管理md

--- src/utils/markdownurls.ts
+++ src/utils/markdownurls.ts
@@ -1,1 +1,11 @@
-export default {};
+export default {
+    "vue-simple-global-state-store-manager":
+        "https://masx200.github.io/vue-simple-global-state-store-manager/README.md",
+
+    "webpack-react-vue-spa-awesome-config":
+        "https://masx200.github.io/webpack-react-vue-spa-awesome-config/README.md",
+    "excellent-vscode-extensions-for-javascript":
+        "https://masx200.github.io/excellent-vscode-extensions-for-javascript/README.md",
+    "react-simple-global-state-store-hook":
+        "https://masx200.github.io/react-simple-global-state-store-hook/README.md",
+};
