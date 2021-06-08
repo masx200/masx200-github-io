130ac0c753b665902ac4701382e60b3fbe68663f
masx200 <masx200@qq.com>
2021-06-08 16:38:05 +0800

    1 file modified

--- src/components/首页推荐网址目录.js
+++ src/components/首页推荐网址目录.js
@@ -1,9 +1,9 @@
 export default [
     ["https://github.com/masx200", "masx200的github仓库"],
-    ["https://weibo.com/2174458781", "一生忽而得一夏当司掌好年华的微博"],
+   
     [
-        "https://space.bilibili.com/27919054",
-        "一生忽得夏司掌好年华的哔哩哔哩个人空间",
+        "https://masx200.github.io/my-vue-router-project/",
+        "masx200的基于vue的首页",
     ],
     [
         "https://masx200.github.io/typescript-book-chinese/",
