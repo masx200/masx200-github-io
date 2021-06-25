323f1cef3ac210fd87b2258b632fa5fe87fee855
masx200 <masx200@qq.com>
2021-06-25 11:12:46 +0800

    Update rssfeedxml.js

--- src/components/my-react-router-test/rssfeedxml.js
+++ src/components/my-react-router-test/rssfeedxml.js
@@ -1,13 +1,14 @@
-export const rssxml1 =
-    "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.2.2/src/my-react-router-test/www.tmtpost.com.rss.xml";
+const rssbase="https://service-dljg106l-1258751639.cd.apigw.tencentcs.com/"
+export const rssxml1 =rssbase+"www.tmtpost.com/feed/"
+    // "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.2.2/src/my-react-router-test/www.tmtpost.com.rss.xml";
 
-export const rssxml2 =
-    "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.2.2/src/my-react-router-test/feed.iplaysoft.com.xml";
-export const rssxml3 =
-    "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.2.2/src/my-react-router-test/landiannews.com.feed.xml";
-export const rssxml4 =
-    "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.2.2/src/my-react-router-test/www.ithome.com.rss.xml";
-export const rssxml5 =
-    "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.2.2/src/my-react-router-test/ifanr.com.feed.xml";
-export const rssxml6 =
-    "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.2.2/src/my-react-router-test/pingwest.com.feed.xml";
+export const rssxml2 =rssbase+"www.iplaysoft.com/feed/"
+    // "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.2.2/src/my-react-router-test/feed.iplaysoft.com.xml";
+export const rssxml3 =rssbase+"www.landiannews.com/feed/"
+    // "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.2.2/src/my-react-router-test/landiannews.com.feed.xml";
+export const rssxml4 =rssbase+"www.ithome.com/feed/"
+    // "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.2.2/src/my-react-router-test/www.ithome.com.rss.xml";
+export const rssxml5 =rssbase+"www.ifanr.com/feed/"
+    // "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.2.2/src/my-react-router-test/ifanr.com.feed.xml";
+export const rssxml6 =rssbase+"www.pingwest.com/feed/"
+    // "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.2.2/src/my-react-router-test/pingwest.com.feed.xml";
