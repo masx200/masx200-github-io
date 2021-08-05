c4be1d93cc89aefa22f571d797714404d1598cfd
masx200 <34191203+masx200@users.noreply.github.com>
2021-08-05 09:10:24 +0800

    Update usemarkdown.ts

--- src/components/markdown-react/usemarkdown.ts
+++ src/components/markdown-react/usemarkdown.ts
@@ -1,13 +1,14 @@
 import { usereactive } from "./usereactive";
 import { markdownstore } from "./markdownstore";
 import { getmarkdownandsave } from "./getmarkdownandsave";
-
+import { readonly } from "@vue/reactivity";
+const readonlymarkdownstore=readonly(markdownstore)
 export function usemarkdown(src: string) {
     getmarkdownandsave(src);
 
     return usereactive(() => {
-        const data = markdownstore.data.get(src);
-        const error = markdownstore.error.get(src);
+        const data = readonlymarkdownstore.data.get(src);
+        const error = readonlymarkdownstore.error.get(src);
         return { data, error };
     });
 }
