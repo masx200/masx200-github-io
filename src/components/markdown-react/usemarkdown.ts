f357d59b75b2b5524e731c66f6431a9434e4b217
masx200 <34191203+masx200@users.noreply.github.com>
2021-08-05 09:10:35 +0800

    Update usemarkdown.ts

--- src/components/markdown-react/usemarkdown.ts
+++ src/components/markdown-react/usemarkdown.ts
@@ -2,7 +2,7 @@ import { usereactive } from "./usereactive";
 import { markdownstore } from "./markdownstore";
 import { getmarkdownandsave } from "./getmarkdownandsave";
 import { readonly } from "@vue/reactivity";
-const readonlymarkdownstore=readonly(markdownstore)
+const readonlymarkdownstore = readonly(markdownstore);
 export function usemarkdown(src: string) {
     getmarkdownandsave(src);
 
