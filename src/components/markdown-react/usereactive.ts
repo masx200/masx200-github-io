d5f8c4c24febd3b6c1a3b4ca2029f7344ad87815
masx200 <masx200@qq.com>
2021-08-07 11:41:37 +0800

    优化

--- src/components/markdown-react/usereactive.ts
+++ src/components/markdown-react/usereactive.ts
@@ -1,17 +1,21 @@
 import { useEffect, useState } from "react";
-import { watch, computed } from "@vue/runtime-core";
+import { effect, computed,stop } from "@vue/reactivity";
 export function usereactive<T>(call: () => T, dependency: any[]) {
     const [state, setstate] = useState(call());
 
     useEffect(() => {
         setstate(call);
         const result = computed(call);
-        const stop = watch(result, (value) => {
+        const re = effect(() => {
+            setstate(result.value);
+        });
+
+        /*watch(result, (value) => {
             console.log(value);
             setstate(call());
-        });
+        });*/
         return () => {
-            stop();
+            stop(re);
         };
     }, dependency);
     return state;
