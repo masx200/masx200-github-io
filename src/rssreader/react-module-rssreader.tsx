eed2679affc41f70d27f7a08f1c968ac84c85f9b
masx200 <34191203+masx200@users.noreply.github.com>
2021-08-07 14:18:17 +0800

    Update react-module-rssreader.tsx

--- src/rssreader/react-module-rssreader.tsx
+++ src/rssreader/react-module-rssreader.tsx
@@ -28,58 +28,59 @@ function Rssreader(props: {
         } else {
             document.title = "React router App-" + "rssreader";
         }
-    }, []);
+    }, [sitename]);
     return (
-        <div className="">
-            <h2>异步fetch加载rss阅读器演示</h2>
-            <p>使用fast-xml-parser把xml转换成json</p>
-            <nav className="navbar navbar-expand-sm bg-light navbar-light ">
-                <ul className="demo">
-                    <Link
-                        data-loading-icon="mui-spinner mui-spinner-custom"
-                        className="mui-btn mui-btn-royal mui-btn-outlined btn-lg"
-                        to={{ p: "/react-rssreader", sitename: "tmtpost" }}
-                    >
-                        加载tmtpost
-                    </Link>
-                    <Link
-                        data-loading-icon="mui-spinner mui-spinner-custom"
-                        className="mui-btn mui-btn-primary mui-btn-outlined btn-lg"
-                        to={{ p: "/react-rssreader", sitename: "iplaysoft" }}
-                    >
-                        加载iplaysoft
-                    </Link>
-                    <Link
-                        data-loading-icon="mui-spinner mui-spinner-custom"
-                        className="mui-btn mui-btn-warning mui-btn-outlined btn-lg"
-                        to={{ p: "/react-rssreader", sitename: "landiannews" }}
-                    >
-                        加载landiannews
-                    </Link>
-                    <Link
-                        data-loading-icon="mui-spinner mui-spinner-custom"
-                        className="mui-btn mui-btn-danger mui-btn-outlined btn-lg"
-                        to={{ p: "/react-rssreader", sitename: "ithome" }}
-                    >
-                        加载ithome
-                    </Link>
-                    <Link
-                        to={{ p: "/react-rssreader", sitename: "ifanr" }}
-                        data-loading-icon="mui-spinner mui-spinner-custom"
-                        className="mui-btn mui-btn-success mui-btn-outlined btn-lg"
-                    >
-                        加载ifanr
-                    </Link>
-                    <Link
-                        data-loading-icon="mui-spinner mui-spinner-custom"
-                        className="mui-btn mui-btn-primary mui-btn-outlined btn-lg"
-                        to={{ p: "/react-rssreader", sitename: "pingwest" }}
-                    >
-                        加载pingwest
-                    </Link>
-                </ul>
-            </nav>
-            {rssfeedurl ? <Rssviewer src={rssfeedurl} /> : <React.Fragment />}
+        <div className=""> {rssmain}
+              {rssfeedurl ? <Rssviewer src={rssfeedurl} /> : <React.Fragment />}
         </div>
     );
 }
+const rssmain =<>   <h2>异步fetch加载rss阅读器演示</h2>
+<p>使用fast-xml-parser把xml转换成json</p>
+<nav className="navbar navbar-expand-sm bg-light navbar-light ">
+    <ul className="demo">
+        <Link
+            data-loading-icon="mui-spinner mui-spinner-custom"
+            className="mui-btn mui-btn-royal mui-btn-outlined btn-lg"
+            to={{ p: "/react-rssreader", sitename: "tmtpost" }}
+        >
+            加载tmtpost
+        </Link>
+        <Link
+            data-loading-icon="mui-spinner mui-spinner-custom"
+            className="mui-btn mui-btn-primary mui-btn-outlined btn-lg"
+            to={{ p: "/react-rssreader", sitename: "iplaysoft" }}
+        >
+            加载iplaysoft
+        </Link>
+        <Link
+            data-loading-icon="mui-spinner mui-spinner-custom"
+            className="mui-btn mui-btn-warning mui-btn-outlined btn-lg"
+            to={{ p: "/react-rssreader", sitename: "landiannews" }}
+        >
+            加载landiannews
+        </Link>
+        <Link
+            data-loading-icon="mui-spinner mui-spinner-custom"
+            className="mui-btn mui-btn-danger mui-btn-outlined btn-lg"
+            to={{ p: "/react-rssreader", sitename: "ithome" }}
+        >
+            加载ithome
+        </Link>
+        <Link
+            to={{ p: "/react-rssreader", sitename: "ifanr" }}
+            data-loading-icon="mui-spinner mui-spinner-custom"
+            className="mui-btn mui-btn-success mui-btn-outlined btn-lg"
+        >
+            加载ifanr
+        </Link>
+        <Link
+            data-loading-icon="mui-spinner mui-spinner-custom"
+            className="mui-btn mui-btn-primary mui-btn-outlined btn-lg"
+            to={{ p: "/react-rssreader", sitename: "pingwest" }}
+        >
+            加载pingwest
+        </Link>
+    </ul>
+</nav>
+</>
