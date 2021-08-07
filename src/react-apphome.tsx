7f94339df7d9376721dd9cdd5567a546f902345f
masx200 <34191203+masx200@users.noreply.github.com>
2021-08-07 14:38:05 +0800

    Update react-apphome.tsx

--- src/react-apphome.tsx
+++ src/react-apphome.tsx
@@ -1,13 +1,13 @@
 // const myvurrouterprojecturl =
 //     "https://my-vue-router-project-masx200.vercel.app/";
 import { useToggle } from "ahooks";
-import React, { useState } from "react";
+import React, { useState ,Suspense,useEffect} from "react";
 // @ts-ignore
 import { initloadingid } from "./initloadingid.ts";
 import Loading from "./Loading";
 import Link from "./CustomLink";
 // @ts-ignore
-import 路由列表 from "./router-react-路由列表.tsx";
+import 路由列表 from "./router-react-路由列表";
 // @ts-ignore
 import 可变路径的rssreader链接 from "./可变路径的rssreader链接.tsx";
 //@ts-ignore
@@ -16,29 +16,15 @@ import 我的自定义导航链接 from "./我的自定义导航链接";
 // @ts-ignore
 import { 调整导航栏和主体的距离 } from "./调整导航栏和主体的距离.js";
 
-var { Suspense } = React;
-var { useEffect } = React;
 export default React.memo(Apphome);
 function Apphome() {
-    const [showhr, setshowhr] = useState(true);
-    const [clientWidth, setclientWidth] = useState(document.body.clientWidth);
+    const [clientWidth, setclientWidth] = useState(window.innerWidth);
     const [state, { toggle }] = useToggle(true);
-    function showhrhandler() {
-        // console.log(document.body.clientWidth,state);
-        if (document.body.clientWidth <= 500) {
-            setshowhr(false);
-        } else {
-            // if (state) {
-            setshowhr(state);
-            // } else {
-            //     setshowhr(false);
-            // }
-        }
-    }
+
     useEffect(() => {
         // console.log("onmounted");
 
-        if (document.body.clientWidth <= 500) {
+        if (window.innerWidth <= 500) {
             shouqi收起折叠的导航栏菜单();
         }
         var initloadele = document.getElementById(initloadingid);
@@ -55,7 +41,7 @@ function Apphome() {
 
         window.addEventListener("resize", () => {
             requestAnimationFrame(() => {
-                setclientWidth(document.body.clientWidth);
+                setclientWidth(window.innerWidth);
                 // showhrhandler();
             });
         });
@@ -64,7 +50,6 @@ function Apphome() {
     }, []);
     useEffect(() => {
         调整导航栏和主体的距离();
-        showhrhandler();
     }, [state, clientWidth]);
 
     function shouqi收起折叠的导航栏菜单() {
@@ -87,14 +72,7 @@ function Apphome() {
                             className="navbar-brand mui-btn mui-btn-primary mui-btn-outlined"
                             to={{}}
                         >
-                            masx200的
-                            <hr
-                                id="hidewidthless500"
-                                style={{
-                                    display: showhr ? "block" : "none",
-                                }}
-                            />
-                            github主页
+                            主页
                         </Link>
                     </div>
                     <button
@@ -117,55 +95,7 @@ function Apphome() {
                                 shouqi收起折叠的导航栏菜单();
                             }}
                         >
-                            <我的自定义导航链接
-                                to={{ p: "/react-home" }}
-                                text={"基于REACT的主页"}
-                            />
-                            <li>
-                                <可变路径的rssreader链接 />
-                            </li>
-                            <我的自定义导航链接
-                                to={{ p: "/react-about" }}
-                                text="关于REACT"
-                            />
-                            <我的自定义导航链接
-                                to={{ p: "/picalc" }}
-                                text="圆周率计算多线程"
-                            />
-
-                            <我的自定义导航链接
-                                to={{ p: "/react-huami" }}
-                                text="花密网页版"
-                            />
-
-                            <我的自定义导航链接
-                                to={{ p: "/decoder" }}
-                                text="JSfuck-and-hieroglyphy-Decoder"
-                            />
-
-                            <我的自定义导航链接
-                                to={{ p: "/jsfuck" }}
-                                text="JSfuck-ENCODER"
-                            />
-
-                            <我的自定义导航链接
-                                to={{ p: "/hieroglyphy" }}
-                                text="hieroglyphy-ENCODER"
-                            />
-
-                            <我的自定义导航链接
-                                to={{
-                                    p: "/webpack-react-vue-spa-awesome-config",
-                                }}
-                                text="webpack-react-vue- 极速零配置的单页面 web 应用打包工具"
-                            />
-
-                            <我的自定义导航链接
-                                to={{
-                                    p: "/excellent-vscode-extensions-for-javascript",
-                                }}
-                                text="VScode的优秀扩展推荐"
-                            />
+                            {navlinkeles}
                         </ul>
                     </div>
                 </nav>
@@ -179,3 +109,44 @@ function Apphome() {
         </div>
     );
 }
+const navlinkeles = (
+    <>
+        <我的自定义导航链接
+            to={{ p: "/react-home" }}
+            text={"基于REACT的主页"}
+        />
+        <li>
+            <可变路径的rssreader链接 />
+        </li>
+        <我的自定义导航链接 to={{ p: "/react-about" }} text="关于REACT" />
+        <我的自定义导航链接 to={{ p: "/picalc" }} text="圆周率计算多线程" />
+
+        <我的自定义导航链接 to={{ p: "/react-huami" }} text="花密网页版" />
+
+        <我的自定义导航链接
+            to={{ p: "/decoder" }}
+            text="JSfuck-and-hieroglyphy-Decoder"
+        />
+
+        <我的自定义导航链接 to={{ p: "/jsfuck" }} text="JSfuck-ENCODER" />
+
+        <我的自定义导航链接
+            to={{ p: "/hieroglyphy" }}
+            text="hieroglyphy-ENCODER"
+        />
+
+        <我的自定义导航链接
+            to={{
+                p: "/webpack-react-vue-spa-awesome-config",
+            }}
+            text="webpack-react-vue- 极速零配置的单页面 web 应用打包工具"
+        />
+
+        <我的自定义导航链接
+            to={{
+                p: "/excellent-vscode-extensions-for-javascript",
+            }}
+            text="VScode的优秀扩展推荐"
+        />
+    </>
+);
