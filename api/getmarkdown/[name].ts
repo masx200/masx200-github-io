35ae695269e12e7958bb1f3ca30e799288e29a94
masx200 <34191203+masx200@users.noreply.github.com>
2021-08-21 14:40:00 +0800

    koa

--- api/getmarkdown/[name].ts
+++ api/getmarkdown/[name].ts
@@ -1,6 +1,21 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
-
+import koa from "koa";
+import cors from "koa-cors";
+import markdownurls from "../../src/utils/markdownurls";
+const app = new koa();
+app.use(cors());
+app.use(async (ctx, next) => {
+    const { name } = ctx.request.query;
+    if (typeof name !== "string") {
+        return next();
+    }
+    if (!Object.keys(markdownurls).includes(name)) {
+        return next();
+    }
+    ctx.body = Reflect.get(markdownurls, name);
+    return;
+});
+const handler = app.callback();
 export default (request: VercelRequest, response: VercelResponse) => {
-    const { name = "World" } = request.query;
-    response.status(200).send(`Hello ${name}!`);
+    handler(request, response);
 };
