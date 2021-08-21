7cd3a6cd0347a8492418bb37ae0078b155f57a0f
masx200 <34191203+masx200@users.noreply.github.com>
2021-08-21 16:22:05 +0800

    webpack-react-vue-spa-awesome-config

--- src/utils/markdownurls.ts
+++ src/utils/markdownurls.ts
@@ -1,10 +1,10 @@
 export default {
     "webpack-react-vue-spa-awesome-config":
-        "https://cdn.jsdelivr.net/gh/masx200/webpack-react-vue-spa-awesome-config/README.md",
+        "https://masx200.github.io/webpack-react-vue-spa-awesome-config/README.md",
     "excellent-vscode-extensions-for-javascript":
         "https://cdn.jsdelivr.net/gh/masx200/excellent-vscode-extensions-for-javascript/README.md",
 
     "masx200-github-io":
         "https://masx200.github.io/masx200-github-io/README.md",
-    //  "https://cdn.jsdelivr.net/gh/masx200/masx200-github-io/README.md",
+    
 };
