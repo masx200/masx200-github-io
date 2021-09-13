78f500874eecb06c8a8eba292d6a14acb91ce052
masx200 <34191203+masx200@users.noreply.github.com>
2021-09-13 15:25:18 +0800

    删除无用链接

--- src/components/首页推荐网址目录.js
+++ src/components/首页推荐网址目录.js
@@ -5,24 +5,24 @@ export default [
         "https://masx200.github.io/my-vue-router-project/",
         "masx200的基于vue的首页",
     ],
-    [
-        "https://masx200.github.io/typescript-book-chinese/",
-        "深入理解 TypeScript,markdown阅读器",
-    ],
+    // [
+    //     "https://masx200.github.io/typescript-book-chinese/",
+    //     "深入理解 TypeScript,markdown阅读器",
+    // ],
     [
         "https://masx200.github.io/markdown-reader/",
         "masx200的 轻量级markdown文档系统阅读器",
     ],
-    [
-        "https://masx200.github.io/javascript-tutorial/",
-        " Javascript教程,markdown阅读器",
-    ],
-    [
-        "https://masx200.github.io/es6tutorial/",
-        "Ecmascript6教程,markdown阅读器",
-    ],
-    [
-        "https://masx200.github.io/typescript-tutorial/",
-        "TypeScript 入门教程 ,markdown阅读器",
-    ],
+    // [
+    //     "https://masx200.github.io/javascript-tutorial/",
+    //     " Javascript教程,markdown阅读器",
+    // ],
+    // [
+    //     "https://masx200.github.io/es6tutorial/",
+    //     "Ecmascript6教程,markdown阅读器",
+    // ],
+    // [
+    //     "https://masx200.github.io/typescript-tutorial/",
+    //     "TypeScript 入门教程 ,markdown阅读器",
+    // ],
 ];
