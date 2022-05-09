fab2141e747ffe9c2a4d99989d7effff1276c05d
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-09 13:39:32 +0800

    undici

--- api/fetch.ts
+++ api/fetch.ts
@@ -1,4 +1,5 @@
-import { fetch } from "undici";
+import * as undici from "undici";
+const fetch = { undici };
 import { VercelRequest, VercelResponse } from "@vercel/node";
 export default (request: VercelRequest, response: VercelResponse) => {
     response.end(fetch.toString());
