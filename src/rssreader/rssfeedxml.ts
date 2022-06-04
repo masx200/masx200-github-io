445dc78b53ab1cf7e4ed1846043157ca9daf1737
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-04 11:39:26 +0800

    rssfeedxml

--- src/rssreader/rssfeedxml.ts
+++ src/rssreader/rssfeedxml.ts
@@ -1,18 +1,16 @@
-const rssbase = "https://service-dljg106l-1258751639.cd.apigw.tencentcs.com/";
+const rssbase = "https://";
 const rssxml1 = rssbase + "www.tmtpost.com/feed/";
-// "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.2.2/src/my-react-router-test/www.tmtpost.com.rss.xml";
 
 const rssxml2 = rssbase + "www.iplaysoft.com/feed/";
-// "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.2.2/src/my-react-router-test/feed.iplaysoft.com.xml";
+
 const rssxml3 = rssbase + "www.landiannews.com/feed/";
-// "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.2.2/src/my-react-router-test/landiannews.com.feed.xml";
+
 const rssxml4 = rssbase + "www.ithome.com/feed/";
-// "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.2.2/src/my-react-router-test/www.ithome.com.rss.xml";
+
 const rssxml5 = rssbase + "www.ifanr.com/feed/";
-// "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.2.2/src/my-react-router-test/ifanr.com.feed.xml";
+
 const rssxml6 = rssbase + "www.pingwest.com/feed/";
-// const rssxml6 = rssbase + "www.pingwest.com/feed/4feszew";
-// "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.2.2/src/my-react-router-test/pingwest.com.feed.xml";
+
 export default {
     tmtpost: rssxml1,
     iplaysoft: rssxml2,
