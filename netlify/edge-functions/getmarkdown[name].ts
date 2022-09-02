ff3f79dc52a351d1b3059ffb986ff8e81dd23902
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-02 09:20:25 +0800

    getrss[name]

--- netlify/edge-functions/getmarkdown[name].ts
+++ netlify/edge-functions/getmarkdown[name].ts
@@ -1,6 +1,9 @@
 import type { Context } from "https://edge.netlify.com";
 export default function (request: Request, context: Context) {
+    const url = new URL(request.url);
+    const path = url.pathname.split("/").at(-1) || "";
     return context.json({
+        path,
         method: request.method,
         headers: Object.fromEntries(request.headers),
         url: request.url,
