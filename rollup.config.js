ed65fd6b61a38e566d8d20d924c1346ef5c5cec0
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-02 11:29:16 +0800

    rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,2 +1,44 @@
 import { defineConfig } from "rollup";
-export default defineConfig([{}, {}]);
+import esbuild from "rollup-plugin-esbuild";
+const plugins = [
+    esbuild({
+        // All options are optional
+        include: /\.[jt]sx?$/, // default, inferred from `loaders` option
+        exclude: /node_modules/, // default
+        sourceMap: true, // default
+        minify: true,
+        target: "esnext", // default, or 'es20XX', 'esnext'
+        jsx: "preserve", // default, or 'preserve'
+        jsxFactory: "React.createElement",
+        jsxFragment: "React.Fragment",
+        // Like @rollup/plugin-replace
+        define: {},
+        tsconfig: "tsconfig.json", // default
+        // Add extra loaders
+        loaders: {
+            // Add .json files support
+            // require @rollup/plugin-commonjs
+            // ".json": "json",
+            // Enable JSX in .js files too
+            // ".js": "jsx",
+        },
+    }),
+];
+export default defineConfig([
+    {
+        input: "./netlify/edge-functions/getmarkdown[name].ts",
+        output: {
+            file: "./netlify/edge-functions/getmarkdown[name].js",
+            format: "esm",
+        },
+        plugins,
+    },
+    {
+        input: "./netlify/edge-functions/getrss[name].ts",
+        output: {
+            file: "./netlify/edge-functions/getrss[name].js",
+            format: "esm",
+        },
+        plugins,
+    },
+]);
