c8d2ad4f1ef91f4c59b0f8a6ac21c6a01475103b
masx200 <34191203+masx200@users.noreply.github.com>
2023-06-16 21:00:28 +0800

    "marked-base-url": "^1.0.1",         "marked-gfm-heading-id": "^3.0.4",         "marked-highlight": "^2.0.1",         "marked-mangle": "^1.0.1",

--- src/components/markdown-react/getrenderedmarkdown.ts
+++ src/components/markdown-react/getrenderedmarkdown.ts
@@ -1,22 +1,25 @@
 "use strict";
-import { marked as parse } from "marked";
 
+//@ts-ignore
+import { DOMPurify } from "./DOMPurify.ts";
 // @ts-ignore
-import hljs from "../../assetsjs/highlight.min.js";
+import { baseUrl } from "marked-base-url";
 //@ts-ignore
 import { cachepromise } from "../../cachepromise.ts";
 //@ts-ignore
-import { DOMPurify } from "./DOMPurify.ts";
-//@ts-ignore
 import { fetchtext } from "./fetchtext.ts";
+import { gfmHeadingId } from "marked-gfm-heading-id";
+// @ts-ignore
+import hljs from "../../assetsjs/highlight.min.js";
+// @ts-ignore
+import { mangle } from "marked-mangle";
+import { marked } from "marked";
+import { markedHighlight } from "marked-highlight";
 //@ts-ignore
 import { myrenderer } from "./myrenderer.ts";
-
-export const getrenderedmarkdown = cachepromise(async function (src: string) {
-    const text = await fetchtext(src);
-    const dirty = parse(text, {
-        renderer: new myrenderer(),
-        baseUrl: src,
+marked.use(
+    markedHighlight({
+        langPrefix: "hljs language-",
         highlight(code, lang) {
             // console.log(code,lang)
             // debugger
@@ -26,6 +29,29 @@ export const getrenderedmarkdown = cachepromise(async function (src: string) {
                 ? hljs.highlight(code, { language: lang }).value
                 : hljs.highlightAuto(code).value;
         },
+    })
+);
+const options = {
+    // prefix: "my-prefix-",
+};
+
+marked.use(mangle());
+marked.use(gfmHeadingId(options));
+export const getrenderedmarkdown = cachepromise(async function (src: string) {
+    marked.use(baseUrl(src));
+    const text = await fetchtext(src);
+    const dirty = marked(text, {
+        renderer: new myrenderer(),
+        // baseUrl: src,
+        // highlight(code, lang) {
+        //     // console.log(code,lang)
+        //     // debugger
+        //     const language = hljs.getLanguage(lang);
+        //     // console.log(language)
+        //     return language
+        //         ? hljs.highlight(code, { language: lang }).value
+        //         : hljs.highlightAuto(code).value;
+        // },
     });
     const clean = DOMPurify.sanitize(dirty);
     return clean;
