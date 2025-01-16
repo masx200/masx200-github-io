8a130934b60a94699ec6af544aa6e57ca6909c33
masx200 <masx200@qq.com>
2025-01-16 10:04:04 +0800

    rssfeedxml

--- src/rssreader/rssfeedxml.ts
+++ src/rssreader/rssfeedxml.ts
@@ -9,7 +9,7 @@ const rssxml4 = rssbase + "www.ithome.com/feed/";
 
 const rssxml5 = rssbase + "www.ifanr.com/feed/";
 
-const rssxml6 = rssbase + "www.pingwest.com/feed/";
+const rssxml6 = "https://weibrss.oneoo.info/rss/7307132662";
 
 export default {
     tmtpost: rssxml1,
@@ -18,4 +18,8 @@ export default {
     ithome: rssxml4,
     ifanr: rssxml5,
     pingwest: rssxml6,
+    小白测评: "https://weibrss.oneoo.info/rss/2022252207",
+    科技美学: "https://weibrss.oneoo.info/rss/1094293345",
+    大米评测: "https://weibrss.oneoo.info/rss/1871821935",
+    酷安网: "https://weibrss.oneoo.info/rss/1746523974",
 };
