0014f57fd4fbe1d9cc994abe86cdbc81f7bf7575
masx200 <masx200@qq.com>
2025-01-16 10:50:52 +0000

    update src/rssreader/rssfeedxml.ts.
    Signed-off-by: masx200 <masx200@qq.com>

--- src/rssreader/rssfeedxml.ts
+++ src/rssreader/rssfeedxml.ts
@@ -1,25 +1,16 @@
-const rssbase = "https://";
-const rssxml1 = rssbase + "www.tmtpost.com/feed/";
-
-const rssxml2 = rssbase + "www.iplaysoft.com/feed/";
-
-const rssxml3 = rssbase + "www.landiannews.com/feed/";
-
-const rssxml4 = rssbase + "www.ithome.com/feed/";
-
-const rssxml5 = rssbase + "www.ifanr.com/feed/";
-
-const rssxml6 = "https://weibrss.oneoo.info/rss/7307132662";
 
 export default {
-    tmtpost: rssxml1,
-    iplaysoft: rssxml2,
-    landiannews: rssxml3,
-    ithome: rssxml4,
-    ifanr: rssxml5,
-    pingwest: rssxml6,
-    小白测评: "https://weibrss.oneoo.info/rss/2022252207",
-    科技美学: "https://weibrss.oneoo.info/rss/1094293345",
-    大米评测: "https://weibrss.oneoo.info/rss/1871821935",
-    酷安网: "https://weibrss.oneoo.info/rss/1746523974",
-};
+      tmtpost: 'https://www.tmtpost.com/feed/',
+        iplaysoft: 'https://www.iplaysoft.com/feed/',
+          landiannews: 'https://www.landiannews.com/feed/',
+            ithome: 'https://www.ithome.com/feed/',
+              ifanr: 'https://www.ifanr.com/feed/',
+                pingwest: 'https://weibrss.oneoo.info/rss/7307132662',
+                  '小白测评': 'https://weibrss.oneoo.info/rss/2022252207',
+                    '科技美学': 'https://weibrss.oneoo.info/rss/1094293345',
+                      '大米评测': 'https://weibrss.oneoo.info/rss/1871821935',
+                        '酷安网': 'https://weibrss.oneoo.info/rss/1746523974',
+                          coderjia: 'https://www.coderjia.cn/rss.xml',
+                            appscross: 'https://appscross.com/rss'
+}
+
