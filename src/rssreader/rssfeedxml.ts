ac0dded7ccc27ef598d1fa29a79f600af0d82dd9
masx200 <masx200@qq.com>
2025-01-28 00:11:56 +0800

    sukka

--- src/rssreader/rssfeedxml.ts
+++ src/rssreader/rssfeedxml.ts
@@ -14,4 +14,5 @@ export default {
     vumstar: "https://masx200.github.io/nextjs-masx200-home/rss/vumstar.rss",
     陶辉: "https://weibrss.oneoo.info/rss/2009781822",
     "52nlp": "https://www.52nlp.cn/feed",
+"sukka":"https://blog.skk.moe/atom.xml"
 };
