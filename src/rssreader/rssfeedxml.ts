725e78a8b94b96fcc454d48d29a9483d0e95379a
masx200 <masx200@qq.com>
2025-02-03 20:01:37 +0800

    Update rssfeedxml.ts

--- src/rssreader/rssfeedxml.ts
+++ src/rssreader/rssfeedxml.ts
@@ -14,5 +14,6 @@ export default {
     vumstar: "https://masx200.github.io/nextjs-masx200-home/rss/vumstar.rss",
     陶辉: "https://weibrss.oneoo.info/rss/2009781822",
     "52nlp": "https://www.52nlp.cn/feed",
-"sukka":"https://masx200.github.io/masx200-github-io/rss/sukka/rss.xml"
+    sukka: "https://masx200.github.io/masx200-github-io/rss/sukka/rss.xml",
+    运维世界: "https://www.ywsj365.com/rss.xml",
 };
